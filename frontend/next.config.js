9eef26e54020f11f0e85dde913984cd2570bd3a1
Sunkireddy_Barath <sunkireddybarath07@gmail.com>
2026-04-21 12:22:22 +0530

    feat: add Next.js 14 project config - package.json, tsconfig, next.config

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['uyznvgpxpajdjdxpnogv.supabase.co'],
+  },
+}
+
+module.exports = nextConfig
